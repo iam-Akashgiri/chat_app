f8dd0277b800d80484b02188a4c4e535983285b0
iam-Akashgiri <047a.giri@gmail.com>
2023-09-02 01:43:14 +0530

    pschopg removed

--- chat_proj/settings.py
+++ chat_proj/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-xvl#hgvr6pif#*3u%msnb%f)4qs#-+a04ym^^)2ln7%!6u^=le
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1','localhost']
 
 
@@ -125,9 +124,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
